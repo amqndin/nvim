9827d3d13de07797ebcf84443d4f7c0dd4504485
amandin <123622161+amawdiin@users.noreply.github.com>
2024-05-22 00:42:40 +0600

    config backup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,11 +1,6 @@
-local utils = require "astroui"
-local get_icon = utils.get_icon
 local hop_mappings = {
   ["s"] = { "<cmd>HopChar1<CR>", desc = "Hop to a specific character" },
   ["<S-s>"] = { "<cmd>HopLine<CR>", desc = "Hop to a specific line" },
-}
-local sections = {
-  s = { desc = get_icon("Session", 0, true) .. "Session" },
 }
 
 ---@type LazySpec
@@ -19,34 +14,6 @@ return {
     "CrystalAlpha358/vim-mcfunction",
     ft = { "mcfunction", "bolt" },
   },
-  -- {
-  --   "Shatur/neovim-session-manager",
-  --   dependencies = {
-  --     { "stevearc/resession.nvim", enabled = false },
-  --     {
-  --       "astronvim/astrocore",
-  --       ---@type AstroCoreOpts
-  --       opts = {
-  --         mappings = {
-  --           n = {
-  --             ["<Leader>s"] = sections.s,
-  --             ["<Leader>sl"] = { "<cmd>SessionManager! load_last_session<cr>", desc = "Load last session" },
-  --             ["<Leader>ss"] = { "<cmd>SessionManager! save_current_session<cr>", desc = "Save this session" },
-  --             ["<Leader>sd"] = { "<cmd>SessionManager! delete_session<cr>", desc = "Delete session" },
-  --             ["<Leader>sf"] = { "<cmd>SessionManager! load_session<cr>", desc = "Search sessions" },
-  --             ["<Leader>s."] = {
-  --               "<cmd>SessionManager! load_current_dir_session<cr>",
-  --               desc = "Load current directory session",
-  --             },
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  --   event = "BufWritePost",
-  --   cmd = "SessionManager",
-  --   enabled = vim.g.resession_enabled ~= false,
-  -- },
   {
     "smoka7/hop.nvim",
     dependencies = {
