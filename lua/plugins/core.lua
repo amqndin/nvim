324de5d3c1335e125e97c56e48f83f4cd105bd0c
amandin <123622161+amawdiin@users.noreply.github.com>
2024-05-22 02:52:29 +0600

    config backup
    went back to shatur's session manager. resession requires too many manual actions, and shatur feels like its reading my mind and knows where i want to go.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -85,48 +85,4 @@ return {
       },
     },
   },
-  {
-    "stevearc/resession.nvim",
-    dependencies = {
-      {
-        "AstroNvim/astrocore",
-        opts = function(_, opts)
-          local maps = opts.mappings
-          maps.n["<Leader>s"] = vim.tbl_get(opts, "_map_sections", "S")
-          maps.n["<Leader>sl"] = { function() require("resession").load "Last Session" end, desc = "Load last session" }
-          maps.n["<Leader>ss"] = { function() require("resession").save() end, desc = "Save this session" }
-          maps.n["<Leader>sS"] = {
-            function() require("resession").save(vim.fn.getcwd(), { dir = "dirsession" }) end,
-            desc = "Save this dirsession",
-          }
-          maps.n["<Leader>st"] = { function() require("resession").save_tab() end, desc = "Save this tab's session" }
-          maps.n["<Leader>sd"] = { function() require("resession").delete() end, desc = "Delete a session" }
-          maps.n["<Leader>sD"] =
-            { function() require("resession").delete(nil, { dir = "dirsession" }) end, desc = "Delete a dirsession" }
-          maps.n["<Leader>sf"] = { function() require("resession").load() end, desc = "Load a session" }
-          maps.n["<Leader>sF"] =
-            { function() require("resession").load(nil, { dir = "dirsession" }) end, desc = "Load a dirsession" }
-          maps.n["<Leader>s."] = {
-            function() require("resession").load(vim.fn.getcwd(), { dir = "dirsession" }) end,
-            desc = "Load current dirsession",
-          }
-          opts.autocmds.resession_auto_save = {
-            {
-              event = "VimLeavePre",
-              desc = "Save session on close",
-              callback = function()
-                local buf_utils = require "astrocore.buffer"
-                local autosave = buf_utils.sessions.autosave
-                if autosave and buf_utils.is_valid_session() then
-                  local save = require("resession").save
-                  if autosave.last then save("Last Session", { notify = false }) end
-                  if autosave.cwd then save(vim.fn.getcwd(), { dir = "dirsession", notify = false }) end
-                end
-              end,
-            },
-          }
-        end,
-      },
-    },
-  },
 }
