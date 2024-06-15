d448a5da0a9730abe9fbd971972205f7d8584bb0
amandin <123622161+amawdiin@users.noreply.github.com>
2024-06-15 17:46:45 +0600

    config backup

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -19,12 +19,13 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
+          "json",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
