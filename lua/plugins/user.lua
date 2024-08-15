a9d9ca018c0805cc2ec4c1b935c644e0c19a075c
amandin <123622161+amawdiin@users.noreply.github.com>
2024-08-15 12:01:29 +0600

    move rpc to its own file

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -10,22 +10,6 @@ return {
     ft = { "mcfunction", "bolt" },
   },
   {
-    "andweeb/presence.nvim",
-    event = "VeryLazy",
-    opts = {
-      neovim_image_text = "In Neovim we trust", -- Text displayed when hovered over the Neovim image
-      main_image = "neovim", -- Main image display (either "neovim" or "file")
-      blacklist = { "antbot" }, -- if the current file name, path, or workspace matches rpc is disabled
-      file_assets = {
-        jmc = { "JMC", "https://raw.githubusercontent.com/WingedSeal/jmc/webpage/src/assets/image/jmc_icon192.png" },
-        mcfunction = {
-          "MCFunction",
-          "https://cdn.discordapp.com/emojis/1142345404403425341.webp?size=96&quality=lossless",
-        },
-      },
-    },
-  },
-  {
     "echasnovski/mini.splitjoin",
     keys = {
       { "gs", mode = { "n", "v" } },
